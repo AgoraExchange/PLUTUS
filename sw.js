eb4bf6dd791c8248f0bce9a4b17bcea2f1deea33
Agora <agoraexhq@gmail.com>
2026-05-15 18:26:01 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'plutus-casino-v27';
+const CACHE_NAME = 'plutus-casino-v32';
 const ASSETS = [
   './',
   './index.html',
@@ -10,9 +10,11 @@ const ASSETS = [
   './GameIcons/Chicken.png',
   './GameIcons/CoinFlip.png',
   './GameIcons/Crash.png',
+  './GameIcons/Cups.png',
   './GameIcons/Dice.png',
   './GameIcons/Mines.png',
   './GameIcons/Plinko.png',
+  './GameIcons/Pump.png',
   './GameIcons/Roulette.png',
   './GameIcons/RussianRoulette.png'
 ];
@@ -20,7 +22,7 @@ const ASSETS = [
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(ASSETS))
+      .then(cache => Promise.allSettled(ASSETS.map(asset => cache.add(asset))))
       .then(() => self.skipWaiting())
   );
 });
@@ -39,6 +41,7 @@ self.addEventListener('fetch', event => {
     caches.match(event.request).then(cached => {
       if (cached) return cached;
       return fetch(event.request).then(response => {
+        if (!response || !response.ok) return response;
         const copy = response.clone();
         caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
         return response;
